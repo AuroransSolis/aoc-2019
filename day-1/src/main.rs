cbc20b292e239943f6a05fdd2c42603dda0da7de
Aurorans Solis <primalucegd@gmail.com>
2019-11-30 22:22:29 -0700

    Completed day 1.

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,3 +1,40 @@
+use std::io::{stdin, Read};
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    stdin().read_to_string(&mut input).unwrap();
+    part_1(&input);
+    part_2(&input);
+    println!("{}", get_additional_fuel(1969 / 3 - 2));
+}
+
+fn part_1(input: &str) {
+    println!(
+        "{}",
+        input
+            .lines()
+            // Use saturating sub to avoid wrapping.
+            .map(|line| (line.parse::<usize>().unwrap() / 3).saturating_sub(2))
+            .sum::<usize>()
+    );
+}
+
+fn part_2(input: &str) {
+    println!(
+        "{}",
+        input.lines()
+            .map(|line| get_additional_fuel((line.parse::<usize>().unwrap() / 3).saturating_sub(2)))
+            .sum::<usize>()
+    );
+}
+
+fn get_additional_fuel(fuel: usize) -> usize {
+    // If the amount of additional fuel would be 0 or negative, just return the current amount of
+    // additional fuel.
+    if fuel / 3 <= 2 {
+        fuel
+    } else {
+        // Otherwise, find out how much more we need.
+        fuel + get_additional_fuel(fuel / 3 - 2)
+    }
 }
