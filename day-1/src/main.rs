2a03c0f4b55beece58442142856b60f60ade539e
Aurorans Solis <primalucegd@gmail.com>
2019-12-01 04:30:38 -0700

    Trying out a lookup table.

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,29 +1,44 @@
+mod lookup;
+
+use lookup::LOOKUP;
 use std::fs::File;
 use std::io::{stdin, BufRead, BufReader};
 
 fn main() {
     let mut p1 = 0;
     let mut p2 = 0;
-    stdin().lock().lines().for_each(|line| {
-        let starting_fuel = (line.unwrap().parse::<usize>().unwrap() / 3).saturating_sub(2);
+    stdin().lock().split(b'\n').for_each(|line| {
+        let mut starting_fuel = 0;
+        let mut mul = 1;
+        for c in line.unwrap().into_iter().rev() {
+            starting_fuel += (c as u32 & 15) * mul;
+            mul *= 10;
+        }
+        let starting_fuel = LOOKUP[starting_fuel as usize];
         p1 += starting_fuel;
         p2 += get_additional_fuel(starting_fuel);
     });
+    /*stdin().lock().lines().for_each(|line| {
+        let starting_fuel = (line.unwrap().parse::<u32>().unwrap() / 3).saturating_sub(2);
+        p1 += starting_fuel;
+        p2 += get_additional_fuel(starting_fuel);
+    });*/
     /*BufReader::new(File::open("day-1/large_10m.txt").unwrap()).lines().for_each(|line| {
-        let starting_fuel = (line.unwrap().parse::<usize>().unwrap() / 3).saturating_sub(2);
+        let starting_fuel = (line.unwrap().parse::<u32>().unwrap() / 3).saturating_sub(2);
         p1 += starting_fuel;
         p2 += get_additional_fuel(starting_fuel);
     });*/
     println!("p1: {}\np2: {}", p1, p2);
+    println!("{}", get_additional_fuel(654));
 }
 
-fn get_additional_fuel(fuel: usize) -> usize {
+fn get_additional_fuel(fuel: u32) -> u32 {
     // If the amount of additional fuel would be 0 or negative, just return the current amount of
     // additional fuel.
-    if fuel / 3 <= 2 {
+    if fuel < 9 {
         fuel
     } else {
         // Otherwise, find out how much more we need.
-        fuel + get_additional_fuel(fuel / 3 - 2)
+        fuel + get_additional_fuel(LOOKUP[fuel as usize])
     }
 }
