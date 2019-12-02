fbbbf9d72176529feef0cf6bf1a6f1af158077f7
Aurorans Solis <primalucegd@gmail.com>
2019-12-02 00:58:48 -0700

    Naming change to reflect what the variable actually is. Whoops.

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -6,7 +6,6 @@ mod p2_lookup;
 use libc::{lseek, SEEK_END, mmap, PROT_READ, MAP_SHARED};
 use p1_lookup::P1_LOOKUP;
 use p2_lookup::P2_LOOKUP;
-use std::io::{stdin, BufRead};
 use std::ptr::null_mut;
 use std::slice::from_raw_parts;
 
@@ -16,27 +15,16 @@ fn main() {
     let len = unsafe { lseek(0, 0, SEEK_END) } as usize;
     let chars = unsafe { (mmap(null_mut(), len, PROT_READ, MAP_SHARED, 0, 0) as *const u8) };
     let chars = unsafe { from_raw_parts(chars, len) };
-    let mut starting_fuel = 0;
+    let mut mass = 0;
     for &b in chars.iter() {
         if b == b'\n' {
-            p1 += P1_LOOKUP[starting_fuel];
-            p2 += P2_LOOKUP[starting_fuel];
-            starting_fuel = 0;
+            p1 += P1_LOOKUP[mass];
+            p2 += P2_LOOKUP[mass];
+            mass = 0;
         } else {
-            starting_fuel *= 10;
-            starting_fuel += b as usize & 15;
+            mass *= 10;
+            mass += b as usize & 15;
         }
     }
     println!("p1: {}\np2: {}", p1, p2);
 }
-
-/*fn get_additional_fuel(fuel: u32) -> u32 {
-    // If the amount of additional fuel would be 0 or negative, just return the current amount of
-    // additional fuel.
-    if fuel < 9 {
-        fuel
-    } else {
-        // Otherwise, find out how much more we need.
-        fuel + get_additional_fuel(P1_LOOKUP[fuel as usize])
-    }
-}*/
