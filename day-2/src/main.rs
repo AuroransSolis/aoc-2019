d760c2ed4a82a3b99bc816a2853d823d7b5a3fb8
Aurorans Solis <primalucegd@gmail.com>
2019-12-02 19:26:04 -0700

    Reworked to make it more flexible in case d3 reuses this stuff.

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -1,6 +1,44 @@
 use std::io::{stdin, BufRead};
 use std::process::exit;
 
+#[derive(Clone)]
+struct Program {
+    mem: Vec<usize>,
+    pc: usize,
+}
+
+impl Program {
+    fn new(mem: Vec<usize>) -> Self {
+        Program {
+            mem,
+            pc: 0
+        }
+    }
+
+    fn solve_for(&mut self, verb: usize, noun: usize) -> usize {
+        self.mem[1] = verb;
+        self.mem[2] = noun;
+        while self.step() {}
+        self.mem[0]
+    }
+
+    fn step(&mut self) -> bool {
+        let res = match self.mem[self.pc] {
+            1 => self.mem[self.mem[self.pc + 1]] + self.mem[self.mem[self.pc + 2]],
+            2 => self.mem[self.mem[self.pc + 1]] * self.mem[self.mem[self.pc + 2]],
+            99 => return false,
+            _ => {
+                eprintln!("Encountered invalid opcode.");
+                exit(0);
+            }
+        };
+        let write_addr = self.mem[self.pc + 3];
+        self.mem[write_addr] = res;
+        self.pc += 4;
+        true
+    }
+}
+
 fn main() {
     let memory = stdin().lock().split(b',').map(|section| {
         let mut section = section.unwrap();
@@ -15,70 +53,23 @@ fn main() {
         }
         val
     }).collect::<Vec<usize>>();
-    part_1(memory.clone());
-    part_2(&memory);
+    let program = Program::new(memory);
+    part_1(program.clone());
+    part_2(&program);
 }
 
-fn part_1(mut memory: Vec<usize>) {
-    memory[1] = 12;
-    memory[2] = 2;
-    let mut i = 0;
-    while i < memory.len() {
-        let op = match memory[i] {
-            99 => break,
-            1 => add,
-            2 => mul,
-            _ => {
-                eprintln!("Encountered invalid opcode.");
-                exit(0);
-            }
-        };
-        let r1 = memory[i + 1];
-        let r2 = memory[i + 2];
-        let w = memory[i + 3];
-        let res = op(memory[r1], memory[r2]);
-        memory[w] = res;
-        i += 4;
-    }
-    println!("{}", memory[0]);
+fn part_1(mut program: Program) {
+    println!("{}", program.solve_for(12, 2));
 }
 
-fn part_2(memory: &Vec<usize>) {
-    for i in 0..100 {
-        for j in 0..100 {
-            let mut tmp_memory = memory.clone();
-            tmp_memory[1] = i;
-            tmp_memory[2] = j;
-            let mut n = 0;
-            while n < tmp_memory.len() {
-                let op = match tmp_memory[n] {
-                    99 => break,
-                    1 => add,
-                    2 => mul,
-                    _ => {
-                        eprintln!("Encountered invalid opcode.");
-                        exit(0);
-                    }
-                };
-                let r1 = tmp_memory[n + 1];
-                let r2 = tmp_memory[n + 2];
-                let w = tmp_memory[n + 3];
-                let res = op(tmp_memory[r1], tmp_memory[r2]);
-                tmp_memory[w] = res;
-                n += 4;
-            }
-            if tmp_memory[0] == 19690720 {
-                println!("({}, {}) => {}", i, j, 100 * i + j);
+fn part_2(program: &Program) {
+    for n in 0..100 {
+        for v in 0..100 {
+            let mut program = program.clone();
+            if program.solve_for(n, v) == 19690720 {
+                println!("{}, {} => {}", n, v, 100 * n + v);
                 return;
             }
         }
     }
-}
-
-fn add(a: usize, b: usize) -> usize {
-    a + b
-}
-
-fn mul(a: usize, b: usize) -> usize {
-    a * b
 }
