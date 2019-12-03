f3fe719e472bba7cb883b2ad3fb07b287f8bb377
Aurorans Solis <primalucegd@gmail.com>
2019-12-03 02:45:26 -0700

    Changed a few things here and there, also got free perf from changing to i32.

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -6,13 +6,13 @@ fn main() {
     for line in stdin().lock().lines() {
         let line = line.unwrap();
         let mut points = HashMap::new();
-        points.insert((0, 0), 0);
         let moves = line.split(',').collect::<Vec<_>>();
         let mut x = 0;
         let mut y = 0;
+        let mut steps = 1;
         for m in moves {
             let (d, amt) = m.split_at(1);
-            let amt = amt.parse::<i64>().unwrap();
+            let amt = amt.parse::<i32>().unwrap();
             let instr = match d {
                 "U" => yp1,
                 "R" => xp1,
@@ -22,28 +22,20 @@ fn main() {
             };
             for _ in 0..amt {
                 instr(&mut x, &mut y);
-                let steps_to = points.len();
-                points.insert((x, y), steps_to);
+                points.insert((x, y), steps);
+                steps += 1;
             }
         }
         line_points.push(points);
     }
-    let mut abs_closest = std::i64::MAX;
-    let mut signal_closest = std::usize::MAX;
+    let mut abs_closest = std::i32::MAX;
+    let mut signal_closest = std::i32::MAX;
     for i in 0..line_points.len() - 1 {
         for j in i + 1..line_points.len() {
-            for (&(x, y), &l1_steps_to) in line_points[i].iter() {
-                if x | y == 0 {
-                    continue;
-                } else {
-                    if x.abs() + y.abs() < abs_closest && line_points[j].contains_key(&(x, y)) {
-                        abs_closest = x.abs() + y.abs();
-                    }
-                    if l1_steps_to < signal_closest {
-                        if let Some(&l2_steps_to) = line_points[j].get(&(x, y)) {
-                            signal_closest = signal_closest.min(l1_steps_to + l2_steps_to);
-                        }
-                    }
+            for (&(x, y), &l1_cost) in line_points[i].iter() {
+                if let Some(&l2_cost) = line_points[j].get(&(x, y)) {
+                    abs_closest = abs_closest.min(x.abs() + y.abs());
+                    signal_closest = signal_closest.min(l1_cost + l2_cost);
                 }
             }
         }
@@ -52,18 +44,18 @@ fn main() {
     println!("p2: {}", signal_closest);
 }
 
-fn yp1(_x: &mut i64, y: &mut i64) {
+fn yp1(_x: &mut i32, y: &mut i32) {
     *y += 1;
 }
 
-fn xp1(x: &mut i64, _y: &mut i64) {
+fn xp1(x: &mut i32, _y: &mut i32) {
     *x += 1;
 }
 
-fn ym1(_x: &mut i64, y: &mut i64) {
+fn ym1(_x: &mut i32, y: &mut i32) {
     *y -= 1;
 }
 
-fn xm1(x: &mut i64, _y: &mut i64) {
+fn xm1(x: &mut i32, _y: &mut i32) {
     *x -= 1;
 }
