decf799b8b095066697aee013c014fcc376a1312
Aurorans Solis <primalucegd@gmail.com>
2019-12-03 11:30:57 -0700

    Wrote parallel version of day 2 for shits and giggles.

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -1,5 +1,9 @@
-use std::io::{stdin, BufRead};
+extern crate num_cpus;
+
+use std::io::{stdin, Read};
 use std::process::exit;
+use std::sync::{Arc, Mutex, atomic::{AtomicBool, Ordering}};
+use std::thread;
 
 #[derive(Clone)]
 struct Program {
@@ -40,26 +44,21 @@ impl Program {
 }
 
 fn main() {
-    let memory = stdin().lock().split(b',').map(|section| {
-        let mut section = section.unwrap();
-        if section[section.len() - 1] == b'\n' {
-            section.pop();
-        }
-        let mut val = 0;
-        let mut mul = 1;
-        for byte in section.into_iter().rev() {
-            val += (byte as usize & 15) * mul;
-            mul *= 10;
-        }
-        val
-    }).collect::<Vec<usize>>();
+    let mut memory = String::new();
+    stdin().read_to_string(&mut memory).unwrap();
+    let memory = memory
+        .trim()
+        .split(',')
+        .map(|x| x.parse().unwrap())
+        .collect::<Vec<usize>>();
     let program = Program::new(memory);
     part_1(program.clone());
-    part_2(&program);
+    // part_2(&program);
+    part_2_mt(&program);
 }
 
 fn part_1(mut program: Program) {
-    println!("{}", program.solve_for(12, 2));
+    println!("p1: {}", program.solve_for(12, 2));
 }
 
 fn part_2(program: &Program) {
@@ -72,4 +71,34 @@ fn part_2(program: &Program) {
             }
         }
     }
+}
+
+fn part_2_mt(program: *const Program) {
+    let iterator = Arc::new(Mutex::new((0..100).flat_map(|i| (0..100).map(move |j| (i, j)))));
+    let done = Arc::new(AtomicBool::new(false));
+    let program = program as usize;
+    let threads = (0..num_cpus::get()).map(|_| {
+        let iterator = iterator.clone();
+        let done = done.clone();
+        thread::spawn(move || {
+            let iterator = iterator;
+            let done = done;
+            let program = unsafe { &*(program as *const Program) };
+            loop {
+                if done.load(Ordering::SeqCst) {
+                    break;
+                }
+                let next = iterator.lock().unwrap().next();
+                if let Some((n, v)) = next {
+                    let mut program = program.clone();
+                    if program.solve_for(n, v) == 19690720 {
+                        println!("p2: {}", 100 * n + v);
+                        done.store(true, Ordering::SeqCst);
+                        return;
+                    }
+                }
+            }
+        })
+    }).collect::<Vec<_>>();
+    threads.into_iter().for_each(|thread| thread.join().unwrap());
 }
